e19164635f32ebd1d6c4d682194fe734a7e05236
Sedrak <sedrak@softhenge.com>
2018-10-23 19:31:33 +0400

    Added 3d plotting handling for several cases: put a value and specify it is 3d put a value and a subkey and specify it is 3d put a value with a 3 subalues and specify it is 3d

--- port/python/tools/plotting.py
+++ port/python/tools/plotting.py
@@ -5,6 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.widgets import Button
 from datetime import datetime
 import numbers
+import madara.knowledge
 
 keys = []
 
@@ -29,13 +30,14 @@ class Plotter:
   # points_per_plot indentifies the number of points to keep for plotting,
   # if 0, then no limit, it will keep all the retrieved polints
 
-  def __init__(self, reader, key, subkeys = None, points_per_plot = 0, frames_of_choice=['geo', 'p1_base_stabilized']):
+  def __init__(self, reader, key, subkeys = None, points_per_plot = 0, frames_of_choice=['geo', 'p1_base_stabilized'], plot_to_3d=False):
     if not reader or not key:
       print "Plotter is not constructed properly"
       return
 
     self.has_frames_key = False
     self.is_capnp_type = None
+    self.plot_to_3d = plot_to_3d
     print reader.get_keys()
     if key.startswith(data_reader_interface.frames_prefix):
       self.has_frames_key = True
@@ -93,91 +95,120 @@ class Plotter:
     plt.pause(0.000000001)
 
 
-  # visualizes a certain subkey
+  # visualizes a certain item from the given subkeys list
   # not to be called from outside
   def visualize_subkey(self, value, index):
 
     subkey = self.subkeys[index]
 
+
     # plot all of values inside subkey
 
+
     if self.is_capnp_type:
+      # capnp values have a bit different presentation so they are handled seperately
       self.visualize_capnp_value(value, subkey, index)
       return
 
-    plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
+
+
+    #  parse as 3d if possible
+    plot_to_3d = self.plot_to_3d and (len(subkey) == 3)
+    if plot_to_3d:
+      plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
+                  ylabel=subkey[1][1], zlabel=subkey[2][1], projection='3d')
+      if not (self.values.has_key(index)):
+        self.values[index] = ([], [], [])
+    else:
+      plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
                   ylabel=subkey[1][1])
+      if not (self.values.has_key(index)):
+        self.values[index] = ([], [])
 
-    # if the object is not capnp type it is not expected to be a dictionary
-
-    if not (self.values.has_key(index)):
-      self.values[index] = ([], [])
-
-    if subkey[0][0] == -1:
-      if self.has_frames_key:
-        # simulate as current time
+    current_time = int(
+      (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
+    for i in range(0, len(subkey)):
+      item = subkey[i]
+      if subkey[i][0] == -1:
         current_time = int(
           (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
-        self.values[index][0].append(current_time)
-        self.values[index][1].append(value[subkey[1][0]])
+        self.values[index][i].append(current_time)
+      elif self.has_frames_key:
+        self.values[index][i].append(value[subkey[i][0]])
       else:
-        self.values[index][0].append(value.toi())
-        self.values[index][1].append(value.retrieve_index(subkey[1][0]).to_double())
-
-    else:
-      if self.has_frames_key:
-        self.values[index][0].append(value[subkey[0][0]])
-        self.values[index][1].append(value[subkey[1][0]])
-      else:
-        # self.values[index][0] and self.values[index][1] represent our x and y axes values
-        # index for the x axes value in KR is the [0][0]
-        self.values[index][0].append(value.retrieve_index(subkey[0][0]).to_double())
-        # index for the y axes value in KR is [1][0]
-        self.values[index][1].append(value.retrieve_index(subkey[1][0]).to_double())
+        self.values[index][i].append(value.retrieve_index(subkey[i][0]).to_double())
 
     if self.number_of_points_per_plot > 0 and len(self.values[index][0]) > self.number_of_points_per_plot:
-        self.values[index][0].pop(0)
-        self.values[index][1].pop(0)
-    plt.plot(self.values[index][0], self.values[index][1])
+      self.values[index][0].pop(0)
+      self.values[index][1].pop(0)
+      if plot_to_3d:
+        self.values[index][2].pop(0)
+
+    if plot_to_3d:
+      plt.plot(self.values[index][0], self.values[index][1], self.values[index][2])
+    else:
+      plt.plot(self.values[index][0], self.values[index][1])
 
 
 
   # visualizing capnp is a bit different, parsing keys and setting into the
   def visualize_capnp_value(self, value, subkey, index):
     # the second one is always a valid value
-    print subkey
-    if not isinstance(subkey, tuple):
+    if not isinstance(subkey, list):
       subvalue = self.get_value_for_key(subkey, value)
       # in this case we expect only one subkey, so we visualize that
       self.visualize_key(subvalue, subkey[0])
       return
 
-    subvalue_2 = self.get_value_for_key(subkey[1][0], value)
-
-    #init values lists to be plotted
-    if not (self.values.has_key(index)):
-      self.values[index] = ([], [])
-
-    if subkey[0][0] == -1:
-      # in this case we plot against time and
-      # since we don't have toi here, we plot against current time
-      current_time = int(
-        (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
-
-      self.values[index][0].append(current_time)
+    # parse as 3d if possible
+    plot_to_3d = self.plot_to_3d and (len(subkey) == 3)
+    if plot_to_3d:
+      plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
+                  ylabel=subkey[1][1], zlabel=subkey[2][1], projection='3d')
+      if not (self.values.has_key(index)):
+        self.values[index] = ([], [], [])
     else:
-      # get the x axes value and append it to the appropriate array
-      subvalue_1 = self.get_value_for_key(subkey[0][0], value)
-      self.values[index][0].append(subvalue_1)
-    self.values[index][1].append(subvalue_2)
-    plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
-                ylabel=subkey[1][1])
-    plt.plot(self.values[index][0], self.values[index][1])
+      plt.subplot(self.number_of_rows, self.number_of_columns, index + 1, xlabel=subkey[0][1],
+                  ylabel=subkey[1][1])
+      if not (self.values.has_key(index)):
+        self.values[index] = ([], [])
+
+    current_time = int(
+      (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
+    for i in range(0, len(subkey)):
+      item = subkey[i]
+
+      if subkey[i][0] == -1:
+        current_time = int(
+          (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
+        self.values[index][i].append(current_time)
+      else:
+        subvalue = self.get_value_for_key(item[0], value)
+        self.values[index][i].append(subvalue)
+
+    # if list is longer than the nuber of points we want to have on the plot, just pop the oldest ones
+    if self.number_of_points_per_plot > 0 and len(self.values[index][0]) > self.number_of_points_per_plot:
+      self.values[index][0].pop(0)
+      self.values[index][1].pop(0)
+      if plot_to_3d:
+        # in 3d we have 3rd index as well to be popped
+        self.values[index][2].pop(0)
+
+    if plot_to_3d:
+      plt.plot(self.values[index][0], self.values[index][1], self.values[index][2])
+    else:
+      plt.plot(self.values[index][0], self.values[index][1])
 
 
 
 
-  # visualizes a single key with no subkeys provided
+  # visualizes a single key (or a subkey) with no list of subkeys provided
+  # this is a general way to plot values being called from most of places
+  # handles several cases
+  # 1. if the value is a dictionay,
+  # 2. if the value is a list
+  # 3. if the value is KR array
+  # 4. if the value is number or a KR of a number type
   def visualize_key(self, value, key):
     if not (self.values.has_key(key)):
       self.values[key] = {}
@@ -185,13 +216,25 @@ class Plotter:
     current_time = int(
       (datetime.utcnow() - datetime.utcfromtimestamp(0)).total_seconds() * data_reader_interface.nano_size)
     # if dictionary (can happen for capnp types)
-    if isinstance(value, dict):
 
-      if (len(self.values[key]) == 0):
-        self.adjust_rows_and_columns(len(value.keys()))
+
+
+    # the case when value is a dictionary
+    if isinstance(value, dict):
+      size = len(value.keys())
+      plot_key_to_3d = self.plot_to_3d and (size >= 3)
+
+      if not plot_key_to_3d:
+        if (len(self.values[key]) == 0):
+          self.adjust_rows_and_columns(len(value.keys()))
+        else:
+          self.adjust_rows_and_columns(len(self.values[key]))
       else:
-        self.adjust_rows_and_columns(len(self.values[key]))
+        # keep a list of names to show as axes on 3d
+        labels = []
       index = 1
+
+
       for curr_subkey in value.keys():
         curr_value = value[curr_subkey]
 
@@ -199,26 +242,46 @@ class Plotter:
         if isinstance(curr_value, numbers.Number):
           if not (self.values[key].has_key(curr_subkey)):
             self.values[key][curr_subkey] = ([], [])
+
+          # keeping always value and time lets switching between 3d and non 3d plottings
           self.values[key][curr_subkey][0].append(current_time)
           self.values[key][curr_subkey][1].append(curr_value)
 
           if self.number_of_points_per_plot > 0 and len( self.values[key][curr_subkey][0]) > self.number_of_points_per_plot:
             self.values[key][curr_subkey][0].pop(0)
             self.values[key][curr_subkey][1].pop(0)
-          plt.subplot(self.number_of_rows, self.number_of_columns, index, xlabel='time', ylabel=curr_subkey)
 
-          plt.plot(self.values[key][curr_subkey][0], self.values[key][curr_subkey][1])
+          if not plot_key_to_3d:
+            plt.subplot(self.number_of_rows, self.number_of_columns, index, xlabel='time', ylabel=curr_subkey)
+            plt.plot(self.values[key][curr_subkey][0], self.values[key][curr_subkey][1])
+          else:
+            labels.append(curr_subkey)
+            if index == 3:
+              # 3 values already gathered
+              # for 3d plotting we need 3d only at least for now
+              break
+
           index += 1
+      if plot_key_to_3d:
+        plt.subplot(self.number_of_rows, self.number_of_columns, 1, xlabel=labels[0], ylabel=labels[1], zlabel=labels[2], projection='3d')
+        plt.plot(self.values[key][labels[0]][1], self.values[key][labels[1]][1], self.values[key][labels[2]][1])
+      return
 
-    #if a list type
+
+
+
+    # the case when the value is an array
     elif isinstance(value, list):
       lenght = len(value)
-      self.adjust_rows_and_columns(lenght)
+      plot_key_to_3d = self.plot_to_3d and (lenght >= 3)
+      if not plot_key_to_3d:
+        self.adjust_rows_and_columns(lenght)
+      else:
+        labels = []
       for i in range(0, lenght):
         if not (self.values[key].has_key(i)):
           self.values[key][i] = ([], [])
         y_label = (key + '.' + str(i))
-        plt.subplot(self.number_of_rows, self.number_of_columns, i + 1, xlabel='time', ylabel=y_label)
         self.values[key][i][1].append(value[i])
         self.values[key][i][0].append(current_time)
 
@@ -227,18 +290,40 @@ class Plotter:
           self.values[key][i][0].pop(0)
           self.values[key][i][1].pop(0)
 
-        plt.plot(self.values[key][i][0], self.values[key][i][1])
+        if not plot_key_to_3d:
+          plt.subplot(self.number_of_rows, self.number_of_columns, i + 1, xlabel='time', ylabel=y_label)
+          plt.plot(self.values[key][i][0], self.values[key][i][1])
+        else:
+          labels.append(y_label)
+          if i == 2:
+            # 2 means 3 values already gathered
+            # for 3d plotting we need 3d only at least for now
+            break
+      if plot_key_to_3d:
+        plt.subplot(self.number_of_rows, self.number_of_columns, 1, xlabel=labels[0], ylabel=labels[1], zlabel=labels[2], projection='3d')
+        plt.plot(self.values[key][0][1], self.values[key][1][1], self.values[key][2][1])
+
+      return
+
+
+
+    # if the value is a single number, not a list, not a dictionary and not a KR
+    #elif isinstance(value, numbers.Number):
+    elif isinstance(value, madara.knowledge.KnowledgeRecord) and value.is_array_type():
     # value is KR, check if array type
     # similar to above one, but rather need to read value in a bit different way
-    elif value.is_array_type():
+    #elif value.is_array_type():
       # try to plot all the values of the array against time
       size = value.size()
-      self.adjust_rows_and_columns(size)
+      plot_key_to_3d = self.plot_to_3d and (size >= 3)
+      if not plot_key_to_3d:
+        self.adjust_rows_and_columns(size)
+      else:
+        labels = []
       for i in range(0, size):
         if not (self.values[key].has_key(i)):
           self.values[key][i] = ([], [])
         y_label = (key + '.' + str(i))
-        plt.subplot(self.number_of_rows, self.number_of_columns, i + 1, xlabel='time', ylabel=y_label)
         self.values[key][i][1].append(value.retrieve_index(i).to_double())
         self.values[key][i][0].append(value.toi())
 
@@ -247,28 +332,53 @@ class Plotter:
           self.values[key][i][0].pop(0)
           self.values[key][i][1].pop(0)
 
-        plt.plot(self.values[key][i][0], self.values[key][i][1])
+        if not plot_key_to_3d:
+          plt.subplot(self.number_of_rows, self.number_of_columns, i + 1, xlabel='time', ylabel=y_label)
+          plt.plot(self.values[key][i][0], self.values[key][i][1])
+        else:
+          labels.append(y_label)
+          if i == 2:
+            # 2 means 3 values already gathered
+            # for 3d plotting we need 3d only at least for now
+            break
+      if plot_key_to_3d:
+        plt.subplot(self.number_of_rows, self.number_of_columns, 1, xlabel=labels[0], ylabel=labels[1], zlabel=labels[2], projection='3d')
+        plt.plot(self.values[key][0][1], self.values[key][1][1], self.values[key][2][1])
+
+      return
 
 
-    else:
+    # if KR is a number
+    elif isinstance(value, madara.knowledge.KnowledgeRecord):
       # it should be a number type
-      plt.subplot(self.number_of_rows, self.number_of_columns, 1, ylabel=key, xlabel='time')
-      if not (self.values[key].has_key(key)):
-        # looks redundant having same key in key, but this lets to keep the structure a bit more generic
-        self.values[key][key] = ([], [])
+      # first keep toi
+      #TODO: confirm we want to use toi instead of current_time in general,
+      # because when plotting it might be useful to plot against real/simulated time instead
+      current_time = value.toi()
+      value = value.to_double()
+    # handle the general case when a number or a KR as a number is coming
+    # it should be a number type
+    plt.subplot(self.number_of_rows, self.number_of_columns, 1, ylabel=key, xlabel='time')
+    if not (self.values[key].has_key(key)):
+      # looks redundant having same key in key, but this lets to keep the structure a bit more generic
+      self.values[key][key] = ([], [])
+
+    self.values[key][key][1].append(value)
+    self.values[key][key][0].append(current_time)
+
+    if self.number_of_points_per_plot > 0 and len(self.values[key][key][0]) > self.number_of_points_per_plot:
+      self.values[key][key][0].pop(0)
+      self.values[key][key][1].pop(0)
+
+    plt.plot(self.values[key][key][0], self.values[key][key][1])
 
 
-      self.values[key][key][1].append(value.to_double())
-      self.values[key][key][0].append(value.toi())
-
-      if self.number_of_points_per_plot > 0 and len(self.values[key][key][0]) > self.number_of_points_per_plot:
-        self.values[key][key][0].pop(0)
-        self.values[key][key][1].pop(0)
 
 
 
 
   # adjust number of rows and columns according to the subplots lenght
+  # to not get out of bounds of subplots list in case
   def adjust_rows_and_columns(self, lenght):
 
     rows = math.sqrt(lenght)
--- port/python/tools/plotting_sample.py
+++ port/python/tools/plotting_sample.py
@@ -15,14 +15,42 @@ if len(sys.argv) >= 2:
     #plotters = config_parser.create_plotters_from_config(sys.argv[1])
     # Visualize the data
     reader = DataReaderFromFile('/media/sedrak/OS/SET/projects/shield_schema/shield', '/media/sedrak/OS/SET/projects/SAFE_reliability_rc_v3.4.0__pikachu-16-0004__2018-08-16-09-47-39.stk')
-    #plot1 = Plotter(reader, 'state_estimation.pose_initializer.robot_state', subkeys=[(('pose.pose.orientation.x', 'x'), ('pose.pose.orientation.y', 'y'))], points_per_plot=10)
-    plot2 = Plotter(reader, 'agent.0.location', subkeys=[((0, 'x'), (1, 'y'))], points_per_plot=100)
+    plot1 = Plotter(reader, 'state_estimation.pose_initializer.robot_state', subkeys=[
+        [('pose.pose.orientation.y', 'y'), ('pose.pose.orientation.x', 'x'), (('pose.pose.orientation.z', 'z'))]],
+                    points_per_plot=100, plot_to_3d=True)
+    plot11 = Plotter(reader, 'state_estimation.pose_initializer.robot_state', subkeys=[
+        'pose.pose.orientation'],
+                    points_per_plot=100, plot_to_3d=True)
+    plot12 = Plotter(reader, 'state_estimation.pose_initializer.robot_state', subkeys=[
+        'pose.pose.orientation'],
+                     points_per_plot=100)
+    # plot2 = Plotter(reader, 'agent.0.location', plot_to_3d=False)#, subkeys=[((0, 'x'), (1, 'y'))], points_per_plot=100)
+    # plot3 = Plotter(reader, 'agent.0.location', plot_to_3d=True)  # , subkeys=[((0, 'x'), (1, 'y'))], points_per_plot=100)
+    # plot4 = Plotter(reader, 'agent.0.location',
+    #                 plot_to_3d=False, subkeys=[((0, 'x'), (1, 'y'))], points_per_plot=100)
+    # plot5 = Plotter(reader, 'agent.0.location',
+    #                 plot_to_3d=True, subkeys=[((0, 'x'), (1, 'y'))], points_per_plot=100)
+    # plot6 = Plotter(reader, 'agent.0.location',
+    #                 plot_to_3d=True, subkeys=[[(0, 'x'), (1, 'y'), (2, 'z')]], points_per_plot=100)
+    plot3 = Plotter(reader, 'sensors.bottom_laser.raw.size')#, subkeys=['magHeading'])
 
     while True:
         #for key in plotters:
         #    plotters[key].visualize()
-        #plot1.visualize()
-        plot2.visualize()
+
+        plot1.visualize()
+        plot11.visualize()
+        #plot12.visualize()
+
+        # agent location with different inputs
+        # plot2.visualize()
+        # plot3.visualize()
+        # plot4.visualize()
+        # plot5.visualize()
+        # plot6.visualize()
+
+        # a single value against time
+        plot3.visualize()
     plotters.plot.show()
 else:
     print "Error! no config file specified"
